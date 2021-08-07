38fda97e35ebef3d26556580b23859c3cd9fa51b
juhom205 <80626044+juhom205@users.noreply.github.com>
2021-08-07 12:15:11 +0200

    Updated command

--- events/ping.js
+++ events/ping.js
@@ -3,7 +3,7 @@ const { prefix, color } = require("../config");
 module.exports = (client) => {
     client.on('message', async message => {
         if (message.content.startsWith("<@!" + client.user.id + ">")) {
-            message.reply(`Yessir, its me! My prefix is \`${prefix}\``)
+            message.reply(`<:bot:872774084331712512> | Hello World! My prefix is \`\`dev\`\` -  type dev help for more information (╯°□°）╯︵ ┻━┻`)
         }
     })
 }
